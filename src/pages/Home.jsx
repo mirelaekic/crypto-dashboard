dc910a7eedb09749ed1aa4538cb63bdac8328a83
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-05-04 13:23:02 +0200

    Home page
    Home page with transition and button to the next page, added background image with blur effect

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../style/home.css"
+export default function Home() {
+    return (
+        <div className="container">
+
+      <div  className="bg-text">
+        <button className="enter">COIN</button>
+      </div>
+
+  </div>
+    )
+}
