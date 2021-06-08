df104929d6a6c144b420b407e4f75357509d76db
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-06-08 17:58:20 +0200

    fw

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import React from 'react'
 import "../style/Dashboard.css"
+
 const pageVariants = {
     initial: {
       opacity: 0,
@@ -39,7 +40,9 @@ export default function Dashboard() {
         transition={pageTransition}>
             <section>
                 <h1 className="dashboard-title">Ranking</h1>
-                
+                <ul>
+
+                </ul>
             </section>
         </motion.div>
     )
